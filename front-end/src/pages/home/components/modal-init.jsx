bf7fd4e6b68e7dc0a6ddda8ba29d30e51cad1023
Joaquin Hernandez <joaquin_downhill@hotmail.com>
2021-09-03 23:22:06 -0500

    Modal de newsletter

--- front-end/src/pages/home/components/modal-init.jsx
+++ front-end/src/pages/home/components/modal-init.jsx
@@ -10,7 +10,7 @@ import { ModalHook } from './../../../shared/components/ModalHook';
 
 
 const ModalInit = () => {
-    const [isOpenModal, modalNewsletterOpen, modalNewsletterClose] = ModalHook(false);
+    const [isOpenModal, modalNewsletterOpen, modalNewsletterClose] = ModalHook(true);
     const band = useRef(false);
     
     useEffect(() => {
