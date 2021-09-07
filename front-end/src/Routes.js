328133f1fe1b6744f3d836e9bb9ec1f45f584550
Joaquin Hernandez <joaquin_downhill@hotmail.com>
2021-09-06 21:03:00 -0500

    Cambios menores

--- front-end/src/Routes.js
+++ front-end/src/Routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './core/header/header.jsx';
 import Footer from './core/footer/footer.jsx';
+import SideTab from './core/others/SideTab.jsx';
 // import Cursor from './core/components/cursor.jsx';
 import Home from './pages/home/home.jsx';
 
@@ -11,6 +12,7 @@ function Routes () {
             {/* <Cursor /> */}
             <Header />
             <div className="container-app">
+                <SideTab />
                 <Switch>
                     <Route exact path="/" component={Home}></Route>
                     <Route path="*" />
---
+++ front-end/src/core/others/SideTab.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SideTab = () => {
+    return(
+        <div className="sidetab">
+            <span>BLAAAAAAAAAAAAA</span>
+        </div>
+    );
+}
+
+export default SideTab;
