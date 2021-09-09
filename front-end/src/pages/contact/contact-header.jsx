b8b4c870b6bba22750556e8e6fbdfbfcb19b182d
Joaquin Hernandez <joaquin_downhill@hotmail.com>
2021-09-08 19:55:41 -0500

    Cambios menores

--- front-end/src/pages/contact/contact-header.jsx
+++ front-end/src/pages/contact/contact-header.jsx
@@ -5,6 +5,16 @@ const ContactHeader = () => {
     return (
         <div className="contact-header">
             <h3>JELLOU!</h3>
+            <div className="paragraphs">
+                <p>¿Cuándo</p>
+                <p>Y</p>
+                <p>En que horarios</p>
+                <p>Escribirnos?</p>
+
+                <p>cuando tu quieras</p>
+
+                <p>¡te compensamos tu maravillosa empatía!</p>
+            </div>
         </div>
     );
 }
